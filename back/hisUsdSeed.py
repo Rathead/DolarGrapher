1f8812b82852440a81cfc00256fa5bb17272cf7c
Ubuntu <ubuntu@ip-172-31-9-98.sa-east-1.compute.internal>
2019-06-12 17:25:04 +0000

    Seed Script

--- back/hisUsdSeed.py
+++ back/hisUsdSeed.py
@@ -2,8 +2,11 @@ import requests
 import datetime as dt
 import time
 
+apiHost = '18.231.117.65'
+apiPort = '8000'
+
 urlExt = 'https://mindicador.cl/api/dolar/'
-urlMyApi = 'http://18.231.117.65:8000/back/usd_prices/'
+urlMyApi = 'http://'+ apiHost + ':' + apiPort'/back/usd_prices/'
 hdrs={"Content-Type": "application/json"}
 Emsg=""
 iniDate = dt.date(2018, 1, 1)
@@ -14,36 +17,39 @@ while (iniDate <= today):
     newUrl = urlExt + dateA
     print(newUrl)
 
-    try :
-        resA = requests.get(newUrl)
-        
-        if resA.status_code == 200 :
-#            if resA.json()['serie'] != [] :
-            try:
-                value = resA.json()['serie'][0]['valor']
-            except:
-                iniDate = iniDate + dt.timedelta(days=1)
-                print("Sin datos")
-                time.sleep(3)
-                pass
-            dateB = iniDate.strftime("%Y-%m-%d")
-            data = '{"clp_value": '+ value +', "date": '+ date +' }'
-            try:
-                resB = requests.post(urlMyApi, headers = hdrs, data = data)
-                if resB.status_code != 201 :
-                    print("Error -B- al insertar")
-                    print("status_code " + resB.status_code)
-                    break
-            except:
-                print("Error -A- al insertar")
+#    try :
+    resA = requests.get(newUrl)
 
+    if resA.status_code == 200 :
+        if resA.json()['serie'] != [] :
+#            try:
+            value = resA.json()['serie'][0]['valor']
+#            except:
+            dateB = iniDate.strftime("%Y-%m-%d")
+            data = {'clp_value': value, 'date': dateB }
+#            try:
+            resB = requests.post(urlMyApi, data = data)
+            if resB.status_code != 201 :
+                print("Error -B- al insertar")
+                print("status_code " + str(resB.status_code))
                 break
-        else:
-            print("Error -B- al consultar")
-            break
-    except:
-        print( "Error -A- al consultar")
+            else:
+                print(resB.status_code)
+#            except:
+#                print("Error -A- al insertar")
+#                break
+
+        else :
+            iniDate = iniDate + dt.timedelta(days=1)
+            print("Sin datos")
+            time.sleep(3)
+            pass
+    else:
+        print("Error -B- al consultar")
         break
+#    except:
+#        print( "Error -A- al consultar")
+#        break
 
     iniDate = iniDate + dt.timedelta(days=1)
     time.sleep(3)
